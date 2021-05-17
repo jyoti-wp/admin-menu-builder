2ac5c52e0530d4da779f0e806d8663d57410b91c
jyoti-wp <sim_s006@icloud.com>
2021-05-17 06:35:02 +0530

    first commit

---
+++ menu-builder.php
@@ -0,0 +1,26 @@
+<?php
+
+/** 
+* Menu Builder main file
+* 
+* @package.Menu.Builder
+*/
+
+
+
+/**
+ * Plugin Name:       Menu Builder
+ * Plugin URI:        https://www.jyotishukla.com/Menu-builder/
+ * Description:       This plugin creates Custom post and taxonomies
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Jyoti Shukla
+ * Author URI:        https://www.jyotishukla.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       menu-builder
+ * Domain Path:       /languages
+ */
+require_once 'patterns/register-patterns.php';
+
